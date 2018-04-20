907f7df101cb6aa9cbe9075ba5d3c0b56c301082
T. Winters <info@thomaswinters.be>
2018-04-20 21:25:17 +0200

    Added TwitterLoginUtils

--- src/main/java/be/thomaswinters/twitter/bot/TwitterBot.java
+++ src/main/java/be/thomaswinters/twitter/bot/TwitterBot.java
@@ -1,10 +1,9 @@
 package be.thomaswinters.twitter.bot;
 
+import be.thomaswinters.twitter.bot.util.TwitterLoginUtils;
 import twitter4j.*;
-import twitter4j.conf.ConfigurationBuilder;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -24,24 +23,9 @@ public abstract class TwitterBot {
     }
 
     public TwitterBot(URL propertiesFile) throws IOException {
-        this(getProperties(propertiesFile));
+        this(TwitterLoginUtils.getProperties(propertiesFile));
     }
 
-    public static Twitter getProperties(URL propertiesFile) throws IOException {
-
-        Properties properties = new Properties();
-        InputStream stream = propertiesFile.openStream();
-        properties.load(stream);
-        stream.close();
-
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setOAuthConsumerKey(properties.getProperty("oauth.consumerKey"));
-        cb.setOAuthConsumerSecret(properties.getProperty("oauth.consumerSecret"));
-        cb.setOAuthAccessToken(properties.getProperty("oauth.accessToken"));
-        cb.setOAuthAccessTokenSecret(properties.getProperty("oauth.accessTokenSecret"));
-        return new TwitterFactory(cb.build()).getInstance();
-
-    }
 
     //endregion
 

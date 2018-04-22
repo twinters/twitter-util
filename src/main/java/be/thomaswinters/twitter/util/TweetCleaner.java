e32672d5b3cf2d24ff1dd5f761683c938fca75de
T. Winters <info@thomaswinters.be>
2018-04-22 14:22:40 +0200

    Added new tweet cleaner function

--- src/main/java/be/thomaswinters/twitter/util/TweetCleaner.java
+++ src/main/java/be/thomaswinters/twitter/util/TweetCleaner.java
@@ -1,24 +1,31 @@
 package be.thomaswinters.twitter.util;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class TweetCleaner {
-	private final static Pattern mentionRemover = Pattern.compile("@(\\w+)",
-			Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+    private final static Pattern mentionRemover = Pattern.compile("@(\\w+)",
+            Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
 
-	public static String cleanTweet(String text) {
-		return removeRetweet(removeMentions(text));
-	}
+    public static String cleanTweet(String text) {
+        return removeRetweet(removeMentions(text));
+    }
 
-	public static String removeRetweet(String text) {
-		if (text.startsWith("RT : ")) {
-			return text.substring(5).trim();
-		}
-		return text.trim();
-	}
+    public static String removeRetweet(String text) {
+        if (text.startsWith("RT : ")) {
+            return text.substring(5).trim();
+        }
+        return text.trim();
+    }
 
-	public static String removeMentions(String text) {
-		return mentionRemover.matcher(text).replaceAll("");
-	}
+    public static String removeMentions(String text) {
+        return mentionRemover.matcher(text).replaceAll("");
+    }
 
+    public static String removeHashtagsAndMentions(String text) {
+        return Arrays.asList(text.split(" ")).stream().filter(e -> !e.startsWith("#") && !e.startsWith("@"))
+                .collect(Collectors.joining(" "));
+
+    }
 }

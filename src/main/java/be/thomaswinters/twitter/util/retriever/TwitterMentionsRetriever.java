b82dce99acbc4bcbbedbc1ca4321962ad4a46df9
T. Winters <info@thomaswinters.be>
2018-04-22 12:56:29 +0200

    Added MentionsRetriever

---
+++ src/main/java/be/thomaswinters/twitter/util/retriever/TwitterMentionsRetriever.java
@@ -0,0 +1,32 @@
+package be.thomaswinters.twitter.util.retriever;
+
+import be.thomaswinters.twitter.util.retriever.util.PagingTweetDownloader;
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TwitterMentionsRetriever implements ITweetRetriever {
+
+    private final Twitter twitter;
+
+    public TwitterMentionsRetriever(Twitter twitter) {
+        this.twitter = twitter;
+    }
+
+    @Override
+    public Stream<Status> retrieve(long sinceId) {
+        return new PagingTweetDownloader(this::getMentions).getTweets(sinceId);
+    }
+
+    private List<Status> getMentions(Paging page) {
+        try {
+            return twitter.getMentionsTimeline(page);
+        } catch (TwitterException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

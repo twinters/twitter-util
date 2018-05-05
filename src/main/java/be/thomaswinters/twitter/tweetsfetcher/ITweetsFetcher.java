1571dbc071b34eefa1bd070145862590e833e3dd
T. Winters <info@thomaswinters.be>
2018-05-05 16:41:29 +0200

    Added some default filtering systems to ITweetsFetcher interface

--- src/main/java/be/thomaswinters/twitter/tweetsfetcher/ITweetsFetcher.java
+++ src/main/java/be/thomaswinters/twitter/tweetsfetcher/ITweetsFetcher.java
@@ -1,7 +1,12 @@
 package be.thomaswinters.twitter.tweetsfetcher;
 
+import be.thomaswinters.twitter.tweetsfetcher.filter.FilteredTweetsFetcher;
+import be.thomaswinters.twitter.tweetsfetcher.filter.UserFilteredTweetsFetcher;
 import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public interface ITweetsFetcher {
@@ -9,5 +14,21 @@ public interface ITweetsFetcher {
 
     default Stream<Status> retrieve() {
         return retrieve(1l);
+    }
+
+    default ITweetsFetcher filter(Predicate<Status> filter) {
+        return new FilteredTweetsFetcher(this, filter);
+    }
+
+    default ITweetsFetcher filterOutOwnTweets(Twitter twitter) throws TwitterException {
+        return new UserFilteredTweetsFetcher(this, twitter);
+    }
+
+    default ITweetsFetcher filterOutRetweets() throws TwitterException {
+        return new FilteredTweetsFetcher(this, FilteredTweetsFetcher.RETWEETS_REJECTER);
+    }
+
+    default ITweetsFetcher filterOutReplies() throws TwitterException {
+        return new FilteredTweetsFetcher(this, FilteredTweetsFetcher.REPLY_REJECTER);
     }
 }

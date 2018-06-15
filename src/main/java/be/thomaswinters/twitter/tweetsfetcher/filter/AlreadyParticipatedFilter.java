237d59366a96faca448cd70ca40286e3066e4aae
T. Winters <info@thomaswinters.be>
2018-06-15 21:38:37 +0200

    Updated already participated filter to deal with only having participated a couple of tweets ago

--- src/main/java/be/thomaswinters/twitter/tweetsfetcher/filter/AlreadyParticipatedFilter.java
+++ src/main/java/be/thomaswinters/twitter/tweetsfetcher/filter/AlreadyParticipatedFilter.java
@@ -1,15 +1,10 @@
 package be.thomaswinters.twitter.tweetsfetcher.filter;
 
-import be.thomaswinters.sentence.SentenceUtil;
-import be.thomaswinters.twitter.exception.TwitterUnchecker;
 import be.thomaswinters.twitter.exception.UncheckedTwitterException;
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
-import java.util.concurrent.ExecutionException;
 import java.util.function.Predicate;
 
 /**
@@ -17,22 +12,57 @@ import java.util.function.Predicate;
  */
 public class AlreadyParticipatedFilter implements Predicate<Status> {
     private final Twitter twitter;
-    private final String mentionTag;
+    private final String screenName;
+    private final int maxNumberLookback;
 
-    public AlreadyParticipatedFilter(Twitter twitter) throws TwitterException {
+    public AlreadyParticipatedFilter(Twitter twitter, int maxNumberLookback) throws TwitterException {
         this.twitter = twitter;
-        this.mentionTag = "@" + twitter.getScreenName();
+//        this.mentionTag = "@" + twitter.getScreenName();
+        this.screenName = twitter.getScreenName();
+        this.maxNumberLookback = maxNumberLookback;
     }
 
 
-    private final Cache<Status, Boolean> partOfCache = CacheBuilder.newBuilder().maximumSize(5000).build();
-    private boolean userIsPartOfTweet(Status status) {
-        try {
-            return partOfCache.get(status,
-                    ()-> SentenceUtil.splitOnSpaces(status.getText()).anyMatch(word -> word.equals(mentionTag)));
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
+    public AlreadyParticipatedFilter(Twitter twitter) throws TwitterException {
+        this(twitter, Integer.MAX_VALUE);
+    }
+
+    private boolean hasTweeted(Status status) {
+        return status.getUser().getScreenName().equals(screenName);
+    }
+
+    public boolean hasParticipatedInConversation(Status status) {
+        Status current = status;
+        if (hasTweeted(status)) {
+            return true;
         }
+        int numberChecked = 1;
+        while (current.getInReplyToStatusId() > 0L && numberChecked < maxNumberLookback) {
+            try {
+                current = twitter.showStatus(current.getInReplyToStatusId());
+            } catch (TwitterException e) {
+                // Status removed
+                if (e.getErrorCode() == 144) {
+                    System.out.println("Problems with previous of " + current.getText());
+                    return false;
+                }
+                // Not authorised to view status: blocked?
+                else if (e.getErrorCode() == 179) {
+                    return true;
+                } else {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                    throw new UncheckedTwitterException(e);
+                }
+            }
+            assert current != null;
+            if (hasTweeted(current)) {
+                return true;
+            }
+            numberChecked += 1;
+        }
+        return false;
+
     }
 
     /**
@@ -43,33 +73,6 @@ public class AlreadyParticipatedFilter implements Predicate<Status> {
      */
     @Override
     public boolean test(Status status) {
-        Status current = status;
-        if (userIsPartOfTweet(status)) {
-            return false;
-        }
-        while (current.getInReplyToStatusId() > 0L) {
-            try {
-                current = twitter.showStatus(current.getInReplyToStatusId());
-            } catch (TwitterException e) {
-                // Status removed
-                if (e.getErrorCode() == 144) {
-                    System.out.println("Problems with previous of " + current.getText());
-                    return true;
-                }
-                // Not authorised to view status: blocked?
-                else if (e.getErrorCode() == 179){
-                    return false;
-                } else {
-                    System.out.println(e.getMessage());
-                    e.printStackTrace();
-                    throw new UncheckedTwitterException(e);
-                }
-            }
-            assert current != null;
-            if (userIsPartOfTweet(current)) {
-                return false;
-            }
-        }
-        return true;
+        return !hasParticipatedInConversation(status);
     }
 }

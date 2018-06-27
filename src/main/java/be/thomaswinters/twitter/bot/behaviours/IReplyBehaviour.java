00ec9714bae1f1471350bac1fee72a7b85f3c19d
T. Winters <info@thomaswinters.be>
2018-06-27 02:23:55 +0200

    Created the PostBehaviour and ReplyBehaviour abstractions

---
+++ src/main/java/be/thomaswinters/twitter/bot/behaviours/IReplyBehaviour.java
@@ -0,0 +1,18 @@
+package be.thomaswinters.twitter.bot.behaviours;
+
+import be.thomaswinters.twitter.bot.Tweeter;
+
+/**
+ * Abstraction for a reply behaviour a Twitterbot can have:
+ */
+@FunctionalInterface
+public interface IReplyBehaviour {
+    /**
+     * Trt doing a replying action using the Tweeter object
+     *
+     * @param tweeter   The twitter connection to use, which is necessary as it notifies the action listeners
+     * @return true if the replying action was successful, false otherwise.
+     *      * This is especially important for behaviours used in a cascade or other composites
+     */
+    boolean post(Tweeter tweeter);
+}

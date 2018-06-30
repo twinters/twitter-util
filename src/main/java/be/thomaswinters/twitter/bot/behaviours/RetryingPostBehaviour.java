fe18bf498d93c5c9f6f9bc8df176c0ef644144d5
T. Winters <info@thomaswinters.be>
2018-06-30 23:21:20 +0200

    Made RetryingPostBehaviour more robust

--- src/main/java/be/thomaswinters/twitter/bot/behaviours/RetryingPostBehaviour.java
+++ src/main/java/be/thomaswinters/twitter/bot/behaviours/RetryingPostBehaviour.java
@@ -1,7 +1,9 @@
 package be.thomaswinters.twitter.bot.behaviours;
 
 import be.thomaswinters.twitter.bot.tweeter.ITweeter;
+import be.thomaswinters.twitter.exception.UncheckedTwitterException;
 
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class RetryingPostBehaviour implements IPostBehaviour {
@@ -16,10 +18,33 @@ public class RetryingPostBehaviour implements IPostBehaviour {
     @Override
     public boolean post(ITweeter tweeter) {
         return IntStream.range(0, maxRetries)
-                .mapToObj(x -> innerPostBehaviour.post(tweeter))
+                .mapToObj(x -> robustPost(tweeter))
                 .dropWhile(e -> !e)
                 .findFirst()
                 .orElse(false);
 
+    }
+
+    private boolean robustPost(ITweeter tweeter) {
+        try {
+            return innerPostBehaviour.post(tweeter);
+        } catch (UncheckedTwitterException e) {
+            // Tweet too long
+            if (e.getErrorCode() == 186) {
+                return false;
+            }
+            // If exceeded rate limitation: sleep
+            else if (e.exceededRateLimitation()) {
+                System.out.println("Exceeded Twitter rate limitation: sleeping for 10 minutes");
+                try {
+                    TimeUnit.MINUTES.sleep(10);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+                return false;
+            } else {
+                throw e;
+            }
+        }
     }
 }

9a71cc792698a6cb8f3740ef7aff1841f3aa36da
T. Winters <info@thomaswinters.be>
2018-06-30 23:20:51 +0200

    Added more methods to UncheckedTwitterException

--- src/main/java/be/thomaswinters/twitter/exception/UncheckedTwitterException.java
+++ src/main/java/be/thomaswinters/twitter/exception/UncheckedTwitterException.java
@@ -1,6 +1,7 @@
 package be.thomaswinters.twitter.exception;
 
 import org.jetbrains.annotations.NotNull;
+import twitter4j.HttpResponseCode;
 import twitter4j.RateLimitStatus;
 import twitter4j.TwitterException;
 import twitter4j.TwitterResponse;
@@ -11,7 +12,7 @@ import java.io.PrintWriter;
 /**
  * Class that can be used to wrap checked TwitterException such that it can be used for lambdas
  */
-public class UncheckedTwitterException extends RuntimeException implements TwitterResponse {
+public class UncheckedTwitterException extends RuntimeException implements TwitterResponse, HttpResponseCode {
     private final TwitterException twitterException;
 
     public UncheckedTwitterException(@NotNull TwitterException twitterException) {
@@ -29,4 +30,43 @@ public class UncheckedTwitterException extends RuntimeException implements Twitt
         return twitterException.getAccessLevel();
     }
 
+    public int getStatusCode() {
+        return twitterException.getStatusCode();
+    }
+
+    public int getErrorCode() {
+        return twitterException.getErrorCode();
+    }
+
+    public String getResponseHeader(String name) {
+        return twitterException.getResponseHeader(name);
+    }
+
+    public int getRetryAfter() {
+        return twitterException.getRetryAfter();
+    }
+
+    public boolean isCausedByNetworkIssue() {
+        return twitterException.isCausedByNetworkIssue();
+    }
+
+    public boolean exceededRateLimitation() {
+        return twitterException.exceededRateLimitation();
+    }
+
+    public boolean resourceNotFound() {
+        return twitterException.resourceNotFound();
+    }
+
+    public String getExceptionCode() {
+        return twitterException.getExceptionCode();
+    }
+
+    public String getErrorMessage() {
+        return twitterException.getErrorMessage();
+    }
+
+    public boolean isErrorMessageAvailable() {
+        return twitterException.isErrorMessageAvailable();
+    }
 }
